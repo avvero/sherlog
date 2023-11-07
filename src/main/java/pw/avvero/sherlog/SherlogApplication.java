fd685e74ac28227d6bbed90308594b1ac395018f
belyaev-ay <anton.belyaev@sibit.tech>
2023-11-07 12:39:37 +0700

    first commit

---
+++ src/main/java/pw/avvero/sherlog/SherlogApplication.java
@@ -0,0 +1,13 @@
+package pw.avvero.sherlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SherlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SherlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pw/avvero/sherlog/SherlogApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.avvero.sherlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SherlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
